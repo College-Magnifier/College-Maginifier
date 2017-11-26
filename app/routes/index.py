908672f7efc3dffde1e5e499364623697cd913ed
lizhuo <lizhuogo@gmail.com>
2017-11-25 21:25:14 -0600

    api region

--- app/routes/index.py
+++ app/routes/index.py
@@ -98,8 +98,50 @@ def get_school_coordinates():
 
 @app.route('/vis/get_subject_scores')
 def get_subject_scores():
+    region_condition = ''
+    if 'scale' in request.args:
+        map_scale = request.args['scale']
+        if map_scale == 'united-states':
+            country_code = 'us'
+        elif map_scale == 'canada':
+            country_code = 'ca'
+        elif map_scale == 'united-kingdom':
+            country_code = 'gb'
+        elif map_scale == 'netherlands':
+            country_code = 'nl'
+        elif map_scale == 'germany':
+            country_code = 'de'
+        elif map_scale == 'china':
+            country_code = 'cn'
+        elif map_scale == 'japan':
+            country_code = 'jp'
+        elif map_scale == 'south-korea':
+            country_code = 'kr'
+        elif map_scale == 'australia':
+            country_code = 'au'
+        elif map_scale == 'asia':
+            continent_name = 'as'
+        elif map_scale == 'north-america':
+            continent_name = 'na'
+        elif map_scale == 'south-america':
+            continent_name = 'sa'
+        elif map_scale == 'europe':
+            continent_name = 'eu'
+        elif map_scale == 'africa':
+            continent_name = 'af'
+        elif map_scale == 'oceania':
+            continent_name = 'oc'
+        if map_scale in ['asia', 'north-america', 'south-america', 'europe', 'africa', 'oceania']:
+            region_condition = 'id IN (SELECT id FROM university_geo WHERE `continent_code`="{}")'.format(continent_name)
+        else:
+            region_condition = 'id IN (SELECT id FROM university_geo WHERE `country_code`="{}")'.format(country_code)
+
     cursor = mysql.connect().cursor()
     sql = 'SELECT `id`, `university`, `overall` AS `OVERALL`, `arts` AS `ARTS`, `eng` AS `ENG`, `life_sci` AS `LIFE SCI`, `natural` AS `NATURAL`, `social` AS `SOCIAL` FROM university_subjects WHERE `arts` IS NOT NULL AND `eng` IS NOT NULL AND `life_sci` IS NOT NULL AND `natural` IS NOT NULL AND `social` IS NOT NULL'
+
+    if region_condition:
+        sql += ' AND ' + region_condition
+
     cursor.execute(sql)
     data = cursor.fetchall()
     cursor.close()
