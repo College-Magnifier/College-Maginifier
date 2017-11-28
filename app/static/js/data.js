94a833f8d45bb5fd85a2c191774605d90219de79
lizhuo <lizhuogo@gmail.com>
2017-11-28 14:11:28 -0600

    table done

--- app/static/js/data.js
+++ app/static/js/data.js
@@ -1,19 +1,45 @@
 Vue.config.devtools = true;
 
+var PAGE_LIMIT = 20;
+
 var app = new Vue({
   el: '#app',
   data: {
-    schoolRows: []
+    schoolRows: [],
+    curSchoolRows: [],
+    curPage: 1,
   },
-  create: function() {
+  created: function() {
     this.getScores();
   },
   methods: {
     getScores: function(subject) {
       var _this = this;
+      var dataObj = {};
       $.ajax({
-        
+        method: 'GET',
+        url: '/data/scores',
+        data: dataObj,
+        success: function(resp) {
+          _this.schoolRows = JSON.parse(resp);
+          _this.curSchoolRows = [];
+          _this.curPage = 1;
+          for (var i = 0; i < PAGE_LIMIT; i++) {
+            _this.curSchoolRows.push(_this.schoolRows[i]);
+          }
+        }
       });
+    },
+    changePage: function(pageNum) {
+      this.curPage = pageNum;
+    }
+  },
+  watch: {
+    curPage: function(newVal) {
+      this.curSchoolRows = [];
+      for (var i = 0; i < PAGE_LIMIT; i++) {
+        this.curSchoolRows.push(this.schoolRows[i + (newVal - 1) * PAGE_LIMIT]);
+      }
     }
   }
 });
--- app/static/js/vview.js
+++ app/static/js/vview.js
@@ -5,22 +5,22 @@
 */
 
 vis.vview = function() {
-//
+
   var vview = {};
 
-vview.render = function(id){
-$.ajax({
-  url: '/get_school_descriptions',
-  data: {
-    id: id
-  },
-  type: 'GET',
-  dataType: 'html',
-  success: function (data) {
-    $('#vview .descriptions').html(data);
-  },
-});
-}
+  vview.render = function(id) {
+    $.ajax({
+      url: '/get_school_descriptions',
+      data: {
+        id: id
+      },
+      type: 'GET',
+      dataType: 'html',
+      success: function (data) {
+        $('#vview .descriptions').html(data);
+      },
+    });
+  };
 
   return vview;
 };
