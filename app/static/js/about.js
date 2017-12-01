3928bc3555e4e4a95df578d1338817094255277c
lizhuo <lizhuogo@gmail.com>
2017-11-30 19:17:48 -0600

    about done

---
+++ app/static/js/about.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+  $('#avatar-1').animate({ opacity: 1 }, 1000);
+  $('#intro-1').delay(300).animate({ left: 0, opacity: 1 }, 1000);
+  $('#avatar-2').delay(1000).animate({ opacity: 1 }, 1000);
+  $('#intro-2').delay(1300).animate({ right: 0, opacity: 1 }, 1000);
+  $('#avatar-3').delay(2000).animate({ opacity: 1 }, 1000);
+  $('#intro-3').delay(2300).animate({ left: 0, opacity: 1 }, 1000);
+});
